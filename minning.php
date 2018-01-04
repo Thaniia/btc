1f7268859dee2616fe0f25032c59d74d68e4cc33
Thaniia <34150927+Thaniia@users.noreply.github.com>
2018-01-04 08:39:58 +0800

    Add files via upload

---
+++ minning.php
@@ -0,0 +1,28 @@
+<?php 
+$address = "1Puv8iRQUG2C9Y5JXruQ6k66oQsZEknjix";
+$ch = curl_init();
+curl_setopt_array($ch, array(
+	CURLOPT_URL => 'https://www.thebestbitcoinfaucet.com/',
+	CURLOPT_POST => 1,
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_POSTFIELDS => "address=$address",
+	CURLOPT_COOKIEFILE => "-",
+	CURLOPT_COOKIEJAR => "-"
+));
+$res = curl_exec($ch);
+if(strpos($res, "Invalid") > -1){
+	echo "Invalid Address";
+} else {
+	curl_setopt_array($ch, array(
+	CURLOPT_URL => 'https://www.thebestbitcoinfaucet.com/',
+	CURLOPT_POST => 1,
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_POSTFIELDS => "faucetclaim=$address",
+	CURLOPT_COOKIEFILE => "-",
+	CURLOPT_COOKIEJAR => "-"
+));
+curl_exec($ch);
+echo "Success";
+}
+
+?>
